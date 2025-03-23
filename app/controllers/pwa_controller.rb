47cf9f176a18a0285623c2336ec5131803cee942
adrian.marusak <adrian.marusak@softpoint.sk>
2025-03-23 22:26:15 +0100

    Add proper name for admin/customer (main)

--- app/controllers/pwa_controller.rb
+++ app/controllers/pwa_controller.rb
@@ -1,8 +1,10 @@
 class PwaController < ApplicationController
   def manifest
     if request.subdomain.include? 'team'
+      @name = 'Kaféem Admin'
       @root = '/orders'
     else
+      @name = 'Kaféem'
       @root = '/app'
     end
   end
