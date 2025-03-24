e4f560d517302f55f8adb8522bcd76e66574a7c9
adrian.marusak <adrian.marusak@softpoint.sk>
2025-03-24 22:58:36 +0100

    Add realtime tokens update (main)

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,8 @@
 class Customer < ApplicationRecord
   # self.primary_key = "id"
 
+  after_update_commit -> { broadcast_update_to :balance, target: "balance-#{id}", html: balance }
+
   before_create :generate_uuid_v7
   validates :name, presence: true
 
