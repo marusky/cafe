be54f13adc34e2b0122e499fcf4d4a533bb1b020
adrian.marusak <adrian.marusak@softpoint.sk>
2025-03-24 19:20:02 +0100

    Add missing js package (main)

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "jsqr" # @1.4.0
 pin "qrcode", to: "https://ga.jspm.io/npm:qrcode@1.5.4/lib/browser.js"
+pin "dijkstrajs" # @1.0.3
 pin "@stimulus-components/auto-submit", to: "@stimulus-components--auto-submit.js" # @6.0.0
